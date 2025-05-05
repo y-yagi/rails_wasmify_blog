3e1dc7d93a5bacc8549587512ac33b17fa40b2d2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2025-05-05 17:50:22 +0900

    Run `bin/rails generate scaffold Post title:string date:date body:text`

---
+++ db/migrate/20250505085019_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.date :date
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
